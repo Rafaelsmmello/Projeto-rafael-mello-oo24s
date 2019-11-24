992fd2a779105175b917dd816bd6ee95887b4f38
Rafaelsmmello <54411064+Rafaelsmmello@users.noreply.github.com>
2019-11-24 12:26:33 -0300

    classes fxml adicionadas

--- src/main/java/br/edu/utfpr/pb/oo24s/controller/FXMLPrincipalController.java
+++ src/main/java/br/edu/utfpr/pb/oo24s/controller/FXMLPrincipalController.java
@@ -5,10 +5,18 @@ package br.edu.utfpr.pb.oo24s.controller;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -16,13 +24,61 @@ import javafx.fxml.Initializable;
  * @author Rafael Mello
  */
 public class FXMLPrincipalController implements Initializable {
-
+    
+    @FXML
+    private Button buttonCheckin;
+    @FXML
+    private Button buttonCheckout;
+    @FXML
+    private Button buttonHospedes;
+    @FXML
+    private Button buttonProdutos;
+    @FXML
+    private Button buttonUsuarios;
+    @FXML
+    private Button buttonRelatorio;
+    @FXML
+    private VBox vboxPrincipal;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-    }    
+        this.buttonCheckin.setOnAction(
+            (t) -> {
+                loadCheckin();
+            }
+        );
+    }
+
+    private void loadCheckin() {
+        try {
+            setDataPane(fadeAnimate("/fxml/FXMLReserva.fxml") );
+        } catch (Exception e) {
+            e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(" .: JavaFX :. ");
+            alert.setHeaderText("Atenção, ocorreu um erro!");
+            alert.setContentText("Falha ao abrir a tela de categorias.");
+            alert.showAndWait();
+        }
+    }
+    
+    private void setDataPane(Node node) {
+        vboxPrincipal.getChildren().setAll(node);
+    }
+
+    private VBox fadeAnimate(String url) throws IOException {
+        VBox v = (VBox) FXMLLoader.load(this.getClass().getResource(url));
+        FadeTransition ft = new FadeTransition(Duration.millis(1500));
+        ft.setNode(v);
+        ft.setFromValue(0.1);
+        ft.setToValue(1);
+        ft.setCycleCount(1);
+        ft.setAutoReverse(false);
+        ft.play();
+        return v;
+    }
     
 }
--- src/main/java/br/edu/utfpr/pb/oo24s/main/MainApp.java
+++ src/main/java/br/edu/utfpr/pb/oo24s/main/MainApp.java
@@ -2,20 +2,30 @@ package br.edu.utfpr.pb.oo24s.main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class MainApp extends Application{
 
+public class MainApp extends Application{
+    
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/FXMLPrincipal.fxml"));
+        Parent root = loader.load();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Sistema Hotel");
+        stage.show();
+    }
+    
+    /*public void start(Stage primaryStage) throws Exception {
         VBox root = (VBox) FXMLLoader.load(
          this.getClass()
           .getResource("/fxml/FXMLPrincipal.fxml")
         );
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/fxmlprincipal.css");
+        scene.getStylesheets().add("/styles/fxmlPrincipal.css");
         
         primaryStage.setTitle("Sistema Hotel");
         primaryStage.setScene(scene);
@@ -23,7 +33,7 @@ public class MainApp extends Application{
         primaryStage.show();
         
     }
-
+*/
     public static void main(String[] args) {
         launch(args);
     }
